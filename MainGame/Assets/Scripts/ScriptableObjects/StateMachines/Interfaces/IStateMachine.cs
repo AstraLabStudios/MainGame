f308c8a6cb518a0464fbb79d20165bfdcfd46692
Kobaru <ikobakitsune@gmail.com>
2023-09-27 22:38:05 +0200

    Revert "Merge pull request #4 from AstraLabStudios/features/State-machine-more-abstraction"
    This reverts commit 2106b9e5fd657a264145c246168b48ed0ad4be26, reversing
    changes made to 2b6d8e97a0f42528965cb82143ba499c7d792856.

--- MainGame/Assets/Scripts/ScriptableObjects/StateMachines/Interfaces/IStateMachine.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MainGame.ScriptableObjects.StateMachines.Interfaces
-{
-    public interface IStateMachine<in TObject, out TState>
-        where TObject : IStateMachine<TObject, TState>
-        where TState : IState<TObject, TState>
-    {
-        public void CheckTransition();
-    }
-}
